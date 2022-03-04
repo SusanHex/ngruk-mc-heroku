fa6eb4f0733725efb7d7af4e748ed85119aca033
suehex4027@gmail.com <susan@.localdomain>
2022-03-03 21:31:43 -0700

    added the -O option to mc download

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar -o server.jar
+wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar -O server.jar
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz -O ngrok.tgz
 tar -xzf ngrok.tgz
 ./ngrok authtoken $NGROK_AUTH_TOKEN --log "stdout"
